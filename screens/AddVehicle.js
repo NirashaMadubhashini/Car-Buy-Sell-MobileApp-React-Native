3fac810d3029361a895c6727206016c8f55f1b87
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-09-30 14:34:49 +0530

    update addvehicle

---
+++ screens/AddVehicle.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AddVehicle() {
+  return (
+    <View>
+      <Text>AddVehicle</Text>
+    </View>
+  )
+}
