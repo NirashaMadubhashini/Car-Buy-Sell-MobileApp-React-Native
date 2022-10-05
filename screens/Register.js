41d950b4b6e3a3f3ba0fcead5c3e1c008d2cd6dc
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-10-05 18:01:18 +0530

    update signUp form

--- screens/Register.js
+++ screens/Register.js
@@ -1,47 +1,68 @@
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image,Alert, } from 'react-native'
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image, Alert, } from 'react-native'
 import React, { useState } from 'react'
 
 
-export default function Register({navigation}) {
-
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
+export default function Register({ navigation }) {
   const simpleAlertHandler = () => {
     alert('Register Succefully');
   };
 
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const saveData = () => {
-      fetch('https://jsonplaceholder.typicode.com/posts', {
-          method: 'POST',
-          body: JSON.stringify({
-              fullName: fullName,
-              email: email,
-              phoneNumber: phoneNumber,
-              username: username,
-              password: password,
-          }),
-          headers: {
-              'Content-type': 'application/json; charset=UTF-8',
-          },
+
+  saveUser = async () => {
+
+    if (fullName != "" && email != "" && phoneNumber != "" && username != "" && password != "") {
+      fetch('http://192.168.1.100:8000/users', {
+        method: 'POST',
+        body: JSON.stringify({
+          fullName: fullName,
+          email: email,
+          phoneNumber: phoneNumber,
+          username: username,
+          password: password
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
       })
-          .then((response) => {Alert.alert("Save Saved Successfully !")})
-          .catch((err)=>{Alert.alert("Error occured !")})
+        .then((response) => response.json())
+        .then((json) => {
+          if (json.status === "500") {
+            Alert.alert(json.message);
+          } else {
+            Alert.alert(json.message);
+            clearTextFields();
+          }
+        })
+        .catch((err) => Alert.alert(err));
+    } else {
+      Alert.alert("Please fill all the fields and try again.")
+    }
+    }
+
+
+  const clearTextFields = () => {
+    setFullName("");
+    setEmail("")
+    setPhoneNumber("");
+    setUsername("");
+    setPassword("");
   }
 
 
 
 
-  
+
   return (
     <View style={styles.container}>
-      <Text style={{fontSize:35,justifyContent: 'center',color:"black",fontWeight:'bold',paddingTop: '15%',fontFamily: 'Roboto'}}>Register</Text>
-      <Image style={styles.tinyLogo}source={require('../assets/icon/Register.png')} />
-      <TextInput style={styles.input1}  value={fullName} onChangeText={(e) => { setFullName(e) }} placeholder='Full Name'/>
+      <Text style={{ fontSize: 35, justifyContent: 'center', color: "black", fontWeight: 'bold', paddingTop: '10%', fontFamily: 'Roboto' }}>Register</Text>
+      <Image style={styles.tinyLogo} source={require('../assets/icon/Register.png')} />
+      <TextInput style={styles.input1} value={fullName} onChangeText={(e) => {setFullName(e)}} placeholder='Full Name' />
       <TextInput style={styles.input2} value={email} onChangeText={(e) => { setEmail(e) }} placeholder='Email' />
       <TextInput style={styles.input2} value={phoneNumber} onChangeText={(e) => { setPhoneNumber(e) }} placeholder='Phone Number' />
       <TextInput style={styles.input2} value={username} onChangeText={(e) => { setUsername(e) }} placeholder='Username' />
@@ -49,9 +70,23 @@ export default function Register({navigation}) {
       <TouchableOpacity
         style={styles.btn}
         // onPress={()=>{navigation.navigate("Login"),{saveData},{simpleAlertHandler}}}
-        title="Simple Alert" onPress={simpleAlertHandler}
-        >
+        onPress={() => { saveUser()}}
+      >
         <Text style={{ color: '#ffff', fontSize: 20 }}>Sign Up</Text>
+      </TouchableOpacity>
+
+      <Text style={{ fontSize: 18,color: "black", paddingTop: '3%',left: '-10%'}}>Already have an account?</Text>
+      <TouchableOpacity
+        style={styles.btn2}
+        onPress={() => {
+          try {
+            navigation.navigate("Login")
+          } catch (err) {
+            console.log(err);
+          }
+        }}
+      >
+        <Text style={{ color: 'black', fontSize: 20,fontWeight:'bold' }}>Login</Text>
       </TouchableOpacity>
     </View>
   )
@@ -88,9 +123,21 @@ const styles = StyleSheet.create({
     marginTop: '5%',
     borderRadius: 100
   },
+  btn2: {
+    width: '60%',
+    padding: 5,
+    // backgroundColor: "#0EAF52",
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '-10%',
+    marginLeft:'55%',
+    borderRadius: 100
+  },
   tinyLogo: {
     marginTop: '10%',
-    width: 80,
-    height: 80,
-  }
+    width: 70,
+    height: 70,
+  },
+ 
 });
